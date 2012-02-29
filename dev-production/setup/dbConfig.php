bb1d0e8d107448e149138783a160745034e23eec
Rohit Sanbhadti <rohits47@gmail.com>
2012-02-28 21:46:06 -0800

    Added method stubs and documentation in generalController, implementation remaining. Made some changes to dbConfig design, added timestamping and username logging for teamInfo updating and submitting.
    Signed-off-by: Rohit Sanbhadti <rohits47@gmail.com>

--- dev-production/setup/dbConfig.php
+++ dev-production/setup/dbConfig.php
@@ -16,22 +16,18 @@ $users[] = array("UserID", "int", "NOT NULL", "AUTO_INCREMENT");
 $users[] = array("PRIMARY KEY(UserID)", "");
 $users[] = array("Username", "TEXT");
 $users[] = array("UserFullName", "TINYTEXT");
-//$users[] = array("UserDescription", "TEXT"); // needed?
 $users[] = array("UserPhoneNumber", "TINYTEXT");
 $users[] = array("UserYear", "INT");
-//$users[] = array("UserMomEmail", "TINYTEXT"); // split into mom/dad email?
 $users[] = array("UserParentsEmail", "TINYTEXT"); // split into mom/dad email?
 $users[] = array("UserEmail", "TINYTEXT");
-//$users[] = array("UserTitle", "TINYTEXT"); // needed?
-//$users[] = array("UserPicture", "TINYTEXT"); // needed?
 $users[] = array("UserPassword", "TINYTEXT");
 $users[] = array("ActivationCode", "TINYTEXT");
 $users[] = array("Activated", "INT"); // nonzero val is true
 $users[] = array("UserSubteam", "TINYTEXT"); // vals: Mechanical, Electronics, Programming, Operational
 $users[] = array("UserType", "TINYTEXT"); // vals: Regular, VP, Admin, Mentor
 
-if($dbConfig->createINNODBTable("RoboUsers", $users))
-	echo "Success! Your RoboUsers Table is now set up! <br />";
+if($dbConfig->createINNODBTable("Users", $users))
+	echo "Success! Your Users Table is now set up! <br />";
 
 /**
  * Teams Table
@@ -41,27 +37,30 @@ $teams[] = array("TeamID", "int", "NOT NULL", "AUTO_INCREMENT");
 $teams[] = array("PRIMARY KEY(TeamID)");
 $teams[] = array("TeamName", "TINYTEXT");
 $teams[] = array("TeamNumber", "TINYTEXT");
-$teams[] = array("UserID", "INT");
+$teams[] = array("UserID", "INT"); // UserID of the user who most recently inputted/updated the team info.
+$teams[] = array("Username", "TINYTEXT"); // Username of the user who most recently inputted/updated the team info. Username stored for faster access
+$teams[] = array("NumericDateUpdated", "TINYTEXT"); // format YYYYMMDDHHmm '201109091706'
+$teams[] = array("EnglishDateUpdated", "TINYTEXT"); // fromat 'September 9, 2011 at 5:06 pm'
 	
 if($dbConfig->createINNODBTable("Teams", $teams))
 	echo "Success! Your Teams Table is now set up! <br />";
-if($dbConfig->setRelation("UserHistories", "Users", "UserID"))
+if($dbConfig->setRelation("Teams", "Users", "UserID"))
 	echo "Success! Your Teams and Users Table are now linked via UserID! <br />";
 
 /**
  * CategoryList Table
  */
 $category = array();
-$category[] = array("CategoryListID", "int", "NOT NULL", "AUTO_INCREMENT");
-$category[] = array("PRIMARY KEY(CategoryListID)");
+$category[] = array("TeamInfoID", "int", "NOT NULL", "AUTO_INCREMENT");
+$category[] = array("PRIMARY KEY(TeamInfoID)");
 $category[] = array("TeamID", "INT");
 $category[] = array("CategoryName", "TINYTEXT");
 $category[] = array("CategoryContent", "TEXT");
 	
-if($dbConfig->createINNODBTable("CategoryList", $category))
-	echo "Success! Your CategoryList Table is now set up! <br />";
-if($dbConfig->setRelation("CategoryList", "Teams", "TeamID"))
-	echo "Success! Your CategoryList and Teams Tables are now linked via TeamID! <br />";
+if($dbConfig->createINNODBTable("TeamInfo", $category))
+	echo "Success! Your TeamInfo Table is now set up! <br />";
+if($dbConfig->setRelation("TeamInfo", "Teams", "TeamID"))
+	echo "Success! Your TeamInfo and Teams Tables are now linked via TeamID! <br />";
 
 
 ?>
